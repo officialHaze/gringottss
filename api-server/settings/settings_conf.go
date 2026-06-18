add933fbed55a243f922efef1fbcb657f84a07e9
Moinak Dey <moinak.dey8@gmail.com>
2026-06-18 19:38:49 +0530

    all docker related changes

--- api-server/settings/settings_conf.go
+++ api-server/settings/settings_conf.go
@@ -1,21 +1,22 @@
 package settings
 
 import (
-	"encoding/json"
 	"fmt"
 	"os"
 	"path"
+
+	"github.com/goccy/go-yaml"
 )
 
 func readSettingsConf() (*SettingsConf, error) {
-	filepath := path.Join("settings", "settings.conf.jsonc")
+	filepath := path.Join("settings", "settings.conf.yaml")
 
 	f, err := os.Open(filepath)
 	if err != nil {
 		return nil, fmt.Errorf("error opening %s: %v", filepath, err)
 	}
 
-	dec := json.NewDecoder(f)
+	dec := yaml.NewDecoder(f)
 
 	conf := &SettingsConf{}
 	if err := dec.Decode(conf); err != nil {
@@ -26,6 +27,6 @@ func readSettingsConf() (*SettingsConf, error) {
 }
 
 type SettingsConf struct {
-	Env_File_Name string `json:"env_file_name"` // which env file to use
-	Server_Port   int    `json:"server_port"`
+	Env_File_Name string `yaml:"env_file_name"` // which env file to use
+	Server_Port   int    `yaml:"server_port"`
 }
